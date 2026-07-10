41f18f9790bd4fd0bac4cd99ef642e8af98f792c
Jauhar Eamam <jauhareamam389@gmail.com>
2026-07-11 04:31:31 +0530

    Time: 9 ms (17.65%), Space: 43.1 MB (57.25%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        
+        HashMap<Character, Integer> map =  new HashMap<>();
+
+        int maxPalindrom = 0;
+
+        int maxOddFreq = Integer.MIN_VALUE;
+
+        boolean isOdd = false;
+
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) +1);
+        }
+
+        for(char ch : map.keySet()){
+            int freq = map.get(ch);
+
+            if(freq%2 == 0){
+                maxPalindrom += freq;
+            }else {
+                // maxOddFreq = Math.max(maxOddFreq, freq);
+
+                maxPalindrom += freq-1;
+
+                isOdd = true;
+            }
+        }
+
+        if(isOdd == true){
+            maxPalindrom += 1;
+        }
+
+        return (s.length() == 0 ? 0 : maxPalindrom);
+
+    }
+}
