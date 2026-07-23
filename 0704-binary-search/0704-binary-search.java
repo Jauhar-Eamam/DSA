962a1b135f035bb6210ccb295f0fc71ae0f3367b
Jauhar Eamam <jauhareamam389@gmail.com>
2026-07-23 12:16:28 +0530

    Time: 0 ms (100%), Space: 48.6 MB (13.89%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = (nums.length)-1;
+        int mid = (int) (low+high) / 2;
+
+        while(low <= high) {
+            if(nums[mid] == target) {
+                return mid;
+            }else if(nums[mid] > target){
+                high = mid-1;
+            }else if(nums[mid] < target){
+                low = mid+1;
+            }
+
+            mid = (int) (low+high) / 2;
+        }
+
+        return -1;
+        
+    }
+}
