a224e58d8be5e13bc7490516cd471bfbb27a96d2
Jauhar Eamam <jauhareamam389@gmail.com>
2026-07-27 02:35:33 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,82 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int index = Integer.MAX_VALUE;
+
+        int low = 0;
+
+        int high = nums.length -1;
+        int mid = (low + high) / 2;
+
+        boolean isIndex = false;
+
+        if(nums[0] < nums[high]){
+            while(low <= high){
+                if(nums[mid] < target){
+                    low = mid + 1;
+                }else if(nums[mid] > target){
+                    high = mid - 1;
+                }else {
+                    return mid;
+                }
+
+                mid = (low + high) / 2;
+            }
+
+            return -1;
+        }
+
+        int start = nums[0];
+
+        while(low <= high) {
+
+            if(nums[mid] >= start) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+                index = Math.min(index, mid);
+                isIndex = true;
+            }
+
+            mid = (low + high) / 2;
+        }
+
+        low = 0;
+        high = (isIndex ? index - 1 : nums.length - 1);
+
+        mid = (low + high) /2;
+
+        while(low <= high) {
+            if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }else {
+                return mid;
+            }
+
+            mid = (low + high) / 2;
+        }
+
+        low = (isIndex ? index : 0);
+        high = nums.length - 1;
+
+        mid = (low + high) /2;
+
+        while(low <= high){
+            if(nums[mid] < target){
+                low = mid + 1;
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }else{
+                return mid;
+            }
+
+            mid = (low + high) / 2;
+        }
+
+        return -1;
+
+        
+    }
+}
