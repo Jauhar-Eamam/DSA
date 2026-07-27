907024a540cb9af221bee602aa81febccdc946ae
Jauhar Eamam <jauhareamam389@gmail.com>
2026-07-28 04:05:20 +0530

    Time: 0 ms (100%), Space: 43.9 MB (27.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -34,10 +34,11 @@ class Solution {
                 if (nums[mid] == target) {
                     return mid;
                 } else if (nums[mid] < target) {
-                    if(nums[mid] >= start){
-                    low = mid + 1;
-                    }else {
-                        high = mid - 1;                    }
+                    if (nums[mid] >= start) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
                 } else if (nums[mid] > target) {
 
                     high = mid - 1;
@@ -52,9 +53,9 @@ class Solution {
                 } else if (nums[mid] < target) {
                     low = mid + 1;
                 } else if (nums[mid] > target) {
-                    if(nums[mid] >= start){
-                    low  = mid + 1;
-                    }else {
+                    if (nums[mid] >= start) {
+                        low = mid + 1;
+                    } else {
                         high = mid - 1;
                     }
                 }
@@ -65,68 +66,5 @@ class Solution {
 
         return -1;
 
-        // while (low <= high) {
-        //     if(nums[mid] > target){
-        //         if(start > target) {
-        //             low = mid + 1;
-        //         }else if(start < target) {
-        //             high = mid - 1;
-        //         }else{
-        //             return 0;
-        //         }
-        //     }else if(nums[mid] < target) {
-        //         if(start > nums[mid]){
-
-        //         }
-        //     }
-
-        //     if (nums[mid] >= start) {
-        //         low = mid + 1;
-        //     } else {
-        //         high = mid - 1;
-        //         index = Math.min(index, mid);
-        //         isIndex = true;
-        //     }
-
-        //     mid = (low + high) / 2;
-        // }
-
-        // low = 0;
-        // high = (isIndex ? index - 1 : nums.length - 1);
-
-        // mid = (low + high) / 2;
-
-        // while (low <= high) {
-        //     if (nums[mid] < target) {
-        //         low = mid + 1;
-        //     } else if (nums[mid] > target) {
-        //         high = mid - 1;
-        //     } else {
-        //         return mid;
-        //     }
-
-        //     mid = (low + high) / 2;
-        // }
-
-        // low = (isIndex ? index : 0);
-        // high = nums.length - 1;
-
-        // mid = (low + high) / 2;
-
-        // while (low <= high) {
-        //     if (nums[mid] < target) {
-        //         low = mid + 1;
-        //     } else if (nums[mid] > target) {
-        //         high = mid - 1;
-        //     } else {
-        //         return mid;
-        //     }
-
-        //     mid = (low + high) / 2;
-        // }
-
-        // return -1;
-
     }
-
 }
