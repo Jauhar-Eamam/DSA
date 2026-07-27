7cdd1cf1e10a78cb2a5f3096c3c68ded933443b0
Jauhar Eamam <jauhareamam389@gmail.com>
2026-07-28 04:04:56 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.99%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -5,18 +5,18 @@ class Solution {
 
         int low = 0;
 
-        int high = nums.length -1;
+        int high = nums.length - 1;
         int mid = (low + high) / 2;
 
         boolean isIndex = false;
 
-        if(nums[0] < nums[high]){
-            while(low <= high){
-                if(nums[mid] < target){
+        if (nums[0] < nums[high]) {
+            while (low <= high) {
+                if (nums[mid] < target) {
                     low = mid + 1;
-                }else if(nums[mid] > target){
+                } else if (nums[mid] > target) {
                     high = mid - 1;
-                }else {
+                } else {
                     return mid;
                 }
 
@@ -28,55 +28,105 @@ class Solution {
 
         int start = nums[0];
 
-        while(low <= high) {
+        if (target >= start) {
+            while (low <= high) {
 
-            if(nums[mid] >= start) {
-                low = mid + 1;
-            }else {
-                high = mid - 1;
-                index = Math.min(index, mid);
-                isIndex = true;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    if(nums[mid] >= start){
+                    low = mid + 1;
+                    }else {
+                        high = mid - 1;                    }
+                } else if (nums[mid] > target) {
+
+                    high = mid - 1;
+                }
+
+                mid = (low + high) / 2;
             }
+        } else {
+            while (low <= high) {
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    low = mid + 1;
+                } else if (nums[mid] > target) {
+                    if(nums[mid] >= start){
+                    low  = mid + 1;
+                    }else {
+                        high = mid - 1;
+                    }
+                }
 
-            mid = (low + high) / 2;
-        }
-
-        low = 0;
-        high = (isIndex ? index - 1 : nums.length - 1);
-
-        mid = (low + high) /2;
-
-        while(low <= high) {
-            if(nums[mid] < target){
-                low = mid + 1;
-            }else if(nums[mid] > target){
-                high = mid - 1;
-            }else {
-                return mid;
+                mid = (low + high) / 2;
             }
-
-            mid = (low + high) / 2;
-        }
-
-        low = (isIndex ? index : 0);
-        high = nums.length - 1;
-
-        mid = (low + high) /2;
-
-        while(low <= high){
-            if(nums[mid] < target){
-                low = mid + 1;
-            }else if(nums[mid] > target){
-                high = mid - 1;
-            }else{
-                return mid;
-            }
-
-            mid = (low + high) / 2;
         }
 
         return -1;
 
-        
+        // while (low <= high) {
+        //     if(nums[mid] > target){
+        //         if(start > target) {
+        //             low = mid + 1;
+        //         }else if(start < target) {
+        //             high = mid - 1;
+        //         }else{
+        //             return 0;
+        //         }
+        //     }else if(nums[mid] < target) {
+        //         if(start > nums[mid]){
+
+        //         }
+        //     }
+
+        //     if (nums[mid] >= start) {
+        //         low = mid + 1;
+        //     } else {
+        //         high = mid - 1;
+        //         index = Math.min(index, mid);
+        //         isIndex = true;
+        //     }
+
+        //     mid = (low + high) / 2;
+        // }
+
+        // low = 0;
+        // high = (isIndex ? index - 1 : nums.length - 1);
+
+        // mid = (low + high) / 2;
+
+        // while (low <= high) {
+        //     if (nums[mid] < target) {
+        //         low = mid + 1;
+        //     } else if (nums[mid] > target) {
+        //         high = mid - 1;
+        //     } else {
+        //         return mid;
+        //     }
+
+        //     mid = (low + high) / 2;
+        // }
+
+        // low = (isIndex ? index : 0);
+        // high = nums.length - 1;
+
+        // mid = (low + high) / 2;
+
+        // while (low <= high) {
+        //     if (nums[mid] < target) {
+        //         low = mid + 1;
+        //     } else if (nums[mid] > target) {
+        //         high = mid - 1;
+        //     } else {
+        //         return mid;
+        //     }
+
+        //     mid = (low + high) / 2;
+        // }
+
+        // return -1;
+
     }
+
 }
