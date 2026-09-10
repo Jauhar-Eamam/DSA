2884f259bafc6ac0b4454d796538bd62830111a4
Jauhar Eamam <jauhareamam389@gmail.com>
2026-09-10 18:58:05 +0530

    Time: 2 ms (18.93%), Space: 46.8 MB (61.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,3 +1,6 @@
+
+// By using Queue 
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,27 +18,86 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> nodeList = new ArrayList<>();
 
-        fun(root, nodeList, 0);
+        Queue<TreeNode> q = new LinkedList<>();
 
-        return nodeList;
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        fun(root, q, res);
+        return res;
     }
 
-    public static void fun(TreeNode root, List<List<Integer>> nodeList, int indx) {
-        if(root == null) {
-            return;
+    public static void fun(TreeNode root, Queue<TreeNode> q, List<List<Integer>> res) {
+        q.add(root);
+        while (!q.isEmpty()) {
+            ArrayList<Integer> tempArr = new ArrayList<>();
+
+            int qsize = q.size();
+
+            while (qsize > 0) {
+                TreeNode t = q.poll();
+                tempArr.add(t.val);
+
+                if (t.left != null) {
+                    q.add(t.left);
+                }
+
+                if (t.right != null) {
+                    q.add(t.right);
+                }
+
+                qsize--;
+            }
+
+            res.add(new ArrayList(tempArr));
+
         }
 
-
-
-        if(nodeList.size() <= indx){
-            nodeList.add(new ArrayList<>());
-        }
-
-        fun(root.left, nodeList, indx+1);
-        nodeList.get(indx).add(root.val);
-        fun(root.right, nodeList, indx+1);
         return;
     }
 }
+
+// By using ArrayList Only
+
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> nodeList = new ArrayList<>();
+
+//         fun(root, nodeList, 0);
+
+//         return nodeList;
+//     }
+
+//     public static void fun(TreeNode root, List<List<Integer>> nodeList, int indx) {
+//         if(root == null) {
+//             return;
+//         }
+
+//         if(nodeList.size() <= indx){
+//             nodeList.add(new ArrayList<>());
+//         }
+
+//         fun(root.left, nodeList, indx+1);
+//         nodeList.get(indx).add(root.val);
+//         fun(root.right, nodeList, indx+1);
+//         return;
+//     }
+// }
