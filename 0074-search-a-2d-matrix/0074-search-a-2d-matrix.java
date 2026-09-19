56411d836257af7754f629de83eed06d7b63dcdd
Jauhar Eamam <jauhareamam389@gmail.com>
2026-09-19 21:36:49 +0530

    Time: 0 ms (100%), Space: 44 MB (43.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,49 +1,81 @@
+// with 2 bineary search
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+
+//         int low = 0;
+//         int high = matrix.length - 1;
+
+//         int mid1 = (low + high) /2;
+
+//         int col = matrix[0].length - 1;
+
+//         while(low <= high){
+//             if(target == matrix[mid1][col]){
+//                 return true;
+//             }else if(target > matrix[mid1][col]){
+//                 low = mid1 + 1;
+//             }else {
+//                 if(target == matrix[mid1][0]){
+//                     return true;
+//                 }else if(target  > matrix[mid1][0]){
+//                     break;
+//                 }else {
+//                     high = mid1 - 1;
+//                 }
+//             }
+
+//             mid1 = (low + high) /2;
+//         }
+
+//         low = 0;
+//         high = col;
+
+//         int mid2 = (low + high) / 2;
+
+//         while(low <= high) {
+//             if(target == matrix[mid1][mid2]){
+//                 return true;
+//             }else if(target > matrix[mid1][mid2]){
+//                 low = mid2 + 1;
+//             }else {
+//                 high = mid2 - 1;
+//             }
+
+//             mid2 = (low + high) / 2;
+//         }
+
+//         return false;
+
+//     }  
+// }
+
+
+// optimize: search in 1 bineary search
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
         int low = 0;
-        int high = matrix.length - 1;
+        int colLength = matrix[0].length;
+        int high = (matrix.length * matrix[0].length) - 1;
 
-        int mid1 = (low + high) /2;
-
-        int col = matrix[0].length - 1;
-
-        while(low <= high){
-            if(target == matrix[mid1][col]){
-                return true;
-            }else if(target > matrix[mid1][col]){
-                low = mid1 + 1;
-            }else {
-                if(target == matrix[mid1][0]){
-                    return true;
-                }else if(target  > matrix[mid1][0]){
-                    break;
-                }else {
-                    high = mid1 - 1;
-                }
-            }
-
-            mid1 = (low + high) /2;
-        }
-
-        low = 0;
-        high = col;
-
-        int mid2 = (low + high) / 2;
+        int mid = (low + high) / 2;
 
         while(low <= high) {
-            if(target == matrix[mid1][mid2]){
+            int row = mid / colLength;
+            int col = mid % colLength;
+
+            if(target == matrix[row][col]) {
                 return true;
-            }else if(target > matrix[mid1][mid2]){
-                low = mid2 + 1;
+            }else if(target > matrix[row][col]){
+                low = mid + 1;
             }else {
-                high = mid2 - 1;
+                high = mid - 1;
             }
 
-            mid2 = (low + high) / 2;
+            mid = (low + high) / 2;
         }
 
         return false;
-
-    }  
+    }
 }
